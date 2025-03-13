18615c919c507353f72e9ebef46d85fc7a05a2b2
juliatk <juia_atkeson25@milton.edu>
2025-03-13 12:30:53 -0400

    questions

--- Documents/full stack/json-example-2022-main/index.js
+++ Documents/full stack/json-example-2022-main/index.js
@@ -5,14 +5,67 @@ console.log(data.length);
 var allbooks = data.length;
 
 const readbooks = data.filter(checkIfRead);
-
-
 function checkIfRead(read) {
-    console.log(data[500]["read?"])
-    return read? == "Yes";
+    console.log(read["read?"])
+    if(read["read?"] == "Yes"){
+      return read
+    }
+  }
+  console.log(readbooks.length); 
+  console.log((readbooks.length / data.length) * 100);
+
+
+
+
+  const audiobooks = data.filter(checkIfAudio);
+  function checkIfAudio(audio) {
+    console.log(audio["audio?"])
+    if(audio["audio?"] == "Yes"){
+      return audio
+    }
   }
 
-  console.log(read.length); 
+  console.log(audiobooks.length); 
+
+  function countgenre() {
+    const countgenre = [];
+    data.forEach(books => {
+        if (!countgenre.includes(books.category)) {
+            countgenre.push(books.category);
+        }
+    });
+  
+    return countgenre.length;
+}
+
+console.log(countgenre());
+console.log(countgenre.length);
+
+
+function mostcommongenre(){
+  
+
+
+}
+
+
+const fantasy = data.filter(checkIfFantasy);
+  function checkIfFantasy(fantasy) {
+    console.log(fantasy["category"])
+    if(fantasy["category"] == "Fantasy"){
+      return fantasy
+    }
+  }
+
+  console.log(fantasy.length); 
+
+
+
+
+
+  
+
+
 
 
 // fetch('./rawBooks.json')
